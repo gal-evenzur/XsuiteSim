525c0dc6fa48a7f87931da6aa9eb2cb7f0d40d71
Femboy <galevenzur2@gmail.com>
2025-09-25 10:05:35 +0300

    Basic pytorch dataset

--- Simulation/create_dataset.py
+++ Simulation/create_dataset.py
@@ -1,19 +1,8 @@
 # %% IMPORTS
-import xobjects as xo
-import xtrack as xt
-
-import h5py
 from dataset_funcs import *
 from params import *
-from matplotlib.ticker import AutoMinorLocator
-import numpy as np
 import matplotlib.pyplot as plt
-from copy import deepcopy
 
-
-from scipy import ndimage
-from scipy.spatial.distance import pdist
-from scipy.spatial import ConvexHull
 
 plt.rcParams['image.cmap'] = 'afmhot'
 # np.random.seed(1)
@@ -23,36 +12,31 @@ name = 'q0'
 setting = 'x'  
 magnet_settings = [490, 490.1]
 change_beam = True
-# shift_list = shifts_array_random(shifts, shifts_range, 5, magnet_settings=magnet_settings, is_array=is_array)
+shift_list = shifts_array_random(shifts, shifts_range, 5, magnet_settings=magnet_settings, is_array=is_array)
 
 n = {
     'train': 1,
     'val': 1,
     'test': 1
 }
-# histograms, xedges, yedges = plot_shift_array(shift_list, magnet_settings, name=name, setting=setting, verbose=False,
-#                                               normalize=False)
-# plt.show()
-
-# histograms, xedges, yedges = shifts_to_histogram(shift_list, filename=dat_file, verbose=True,
-#                                                  change_beam=True, normalize=False, std=False, minmax=True)
-
-# # | TRAIN | 
-# shift_train, histogram_train, xedges, yedges = rand_from_scratch_histogram(shifts_template=shifts, shifts_range=shifts_range,
-#                                                          particles_file=dat_file,
-#                                                          n_batch=n['train'], magnet_settings=magnet_settings,
-#                                                          verbose=True,
-#                                                          change_beam=change_beam, normalize=False, std=False, minmax=True)
-# print("Histogram train shape:", histogram_train.shape)
 
 
-# # VAL 
-# shift_val, histogram_val, _, _ = rand_from_scratch_histogram(shifts_template=shifts, shifts_range=shifts_range,
-#                                                          particles_file=dat_file,
-#                                                          n_batch=n['val'], magnet_settings=magnet_settings,
-#                                                          verbose=True,
-#                                                          change_beam=change_beam, normalize=True, std=False, minmax=True)
-# print("Histogram val shape:", histogram_val.shape)
+# | TRAIN | 
+shift_train, histogram_train, xedges, yedges = rand_from_scratch_histogram(shifts_template=shifts, shifts_range=shifts_range,
+                                                         particles_file=dat_file,
+                                                         n_batch=n['train'], magnet_settings=magnet_settings,
+                                                         verbose=True,
+                                                         change_beam=change_beam, normalize=False, std=False, minmax=True)
+print("Histogram train shape:", histogram_train.shape)
+
+
+# VAL 
+shift_val, histogram_val, _, _ = rand_from_scratch_histogram(shifts_template=shifts, shifts_range=shifts_range,
+                                                         particles_file=dat_file,
+                                                         n_batch=n['val'], magnet_settings=magnet_settings,
+                                                         verbose=True,
+                                                         change_beam=change_beam, normalize=True, std=False, minmax=True)
+print("Histogram val shape:", histogram_val.shape)
 
 # < TEST >
 # shift_test, histogram_test, _, _ = rand_from_scratch_histogram(shifts_template=shifts, shifts_range=shifts_range,
@@ -60,8 +44,7 @@ n = {
 #                                                          n_batch=n['test' ], magnet_settings=magnet_settings,
 #                                                          verbose=True,
 #                                                          change_beam=change_beam, normalize=True, std=True, minmax=False)
-
-
+# For testing purposes, use deterministic shifts
 shift_test = shifts_array_deterministic(shifts, 'q0', 'x', [0], magnet_settings=magnet_settings)
 shift_test = shift_list_to_matrix(shift_test, n=num_shifts)
 histogram_test, xedges, yedges = shifts_to_histogram(shift_test, filename=dat_file)
